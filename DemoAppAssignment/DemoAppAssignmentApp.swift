36e59c0e6f02fe61b2e0fa8e820abdf159b76d5c
Sonam71371 <sonam.iimtgn@gmail.com>
2025-01-30 11:20:30 +0100

    Initial Commit

---
+++ DemoAppAssignment/DemoAppAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppAssignmentApp.swift
+//  DemoAppAssignment
+//
+//  Created by Sonam Kumari on 30/01/25.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
